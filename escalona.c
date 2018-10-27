db569db91d8810e8bf32c771540f9950e5f51d00
Schatzmann <annelyse94@gmail.com>
2018-10-27 19:36:47 -0300

    Finalizada a parte de serialização por conflito

--- escalona.c
+++ escalona.c
@@ -49,102 +49,95 @@ t_operacao *leEntrada(int *qtdeOperacoes){
 		return operacoes;
 }
 
-// int serial_conflito(t_operacao *operacoes, int qtdeOperacoes, int **matrizArestas){
+char *verificaSerialConflito(int **matrizArestas, int tamMatriz){
+	int ehSerial = 1;
 
-// 	/* Aresta Ti -> Tj para cada r(x) em Tj depois de w(x) em Ti */
-// 	for(int i=0; i < qtdeOperacoes; i++){
-// 		if(operacoes[i].opr == 'W'){
-// 			for(int j=i + 1; j < qtdeOperacoes; j++){
-// 				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
-// 					break;
-// 				if(operacoes[j].opr == 'R' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
-// 					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
-// 				}
-// 			}
-// 		}
-// 	}
+	printMatriz(matrizArestas, tamMatriz);
 
-// 	/* Aresta Ti -> Tj para cada w(x) em Tj depois de r(x) em Ti */
-// 	for(int i=0; i < qtdeOperacoes; i++){
-// 		if(operacoes[i].opr == 'R'){
-// 			for(int j=i + 1; j < qtdeOperacoes; j++){
-// 				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
-// 					break;
-// 				if(operacoes[j].opr == 'W' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
-// 					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
-// 				}
-// 			}
-// 		}
-// 	}
+	for(int i = 1; i <= tamMatriz; ++i){
+		for(int j = 1; j <= tamMatriz; ++j){
+			if(matrizArestas[i][j] != 0){
+				if(matrizArestas[j][i] != 0){
+					ehSerial = 0;
+				}
+			}
+		}
+	}
 
-// 	/* Aresta Ti -> Tj para cada w(x) em Tj depois de w(x) em Ti */
-// 	for(int i=0; i < qtdeOperacoes; i++){
-// 		if(operacoes[i].opr == 'W'){
-// 			for(int j=i + 1; j < qtdeOperacoes; j++){
-// 				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
-// 					break;
-// 				if(operacoes[j].opr == 'W' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
-// 					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+	if (ehSerial){
+		return "SS";
+	} else {
+		return "NS";
+	}
+}
 
-// int** aloca_matriz(t_operacao *operacoes, int qtdeOperacoes, int *tamMatriz){
-// 	int maiorTransacao = 0;
-// 	int **matrizArestas;
+char *serialConflito(t_operacao *operacoes, int tempoInicio, int tempoFim, int **matrizArestas, int tamMatriz){
 
-// 	for (int i = 0; i < qtdeOperacoes; ++i){
-// 		if(operacoes[i].idT > maiorTransacao){
-// 			maiorTransacao = operacoes[i].idT;
-// 		}
-// 	}
+	/* Aresta Ti -> Tj para cada r(x) em Tj depois de w(x) em Ti */
+	for(int i=tempoInicio; i <= tempoFim; i++){
+		if(operacoes[i].opr == 'W'){
+			for(int j=i + 1; j <= tempoFim; j++){
+				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
+					break;
+				if(operacoes[j].opr == 'R' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
+					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
+				}
+			}
+		}
+	}
 
-// 	maiorTransacao++;
+	/* Aresta Ti -> Tj para cada w(x) em Tj depois de r(x) em Ti */
+	for(int i=tempoInicio; i <= tempoFim; i++){
+		if(operacoes[i].opr == 'R'){
+			for(int j=i + 1; j <= tempoFim; j++){
+				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
+					break;
+				if(operacoes[j].opr == 'W' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
+					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
+				}
+			}
+		}
+	}
 
-// 	matrizArestas = (int**) malloc(maiorTransacao * sizeof(int*));
+	/* Aresta Ti -> Tj para cada w(x) em Tj depois de w(x) em Ti */
+	for(int i=tempoInicio; i <= tempoFim; i++){
+		if(operacoes[i].opr == 'W'){
+			for(int j=i + 1; j <= tempoFim; j++){
+				if(operacoes[j].opr == 'C' && operacoes[i].idT == operacoes[j].idT)
+					break;
+				if(operacoes[j].opr == 'W' && operacoes[i].atrib == operacoes[j].atrib && operacoes[i].idT != operacoes[j].idT){
+					matrizArestas[operacoes[i].idT][operacoes[j].idT] = 1;
+				}
+			}
+		}
+	}
 
-// 	for (int i = 0; i < maiorTransacao; ++i){
-// 		matrizArestas[i] = (int*) malloc(maiorTransacao * sizeof(int));
-// 		for (int j = 0; j < maiorTransacao; ++j){
-// 			matrizArestas[i][j] = 0;
-// 		}
-// 	}
+	return verificaSerialConflito(matrizArestas, tamMatriz);
+}
 
-// 	*tamMatriz = maiorTransacao;
+void printMatriz(int** matrizArestas, int tamMatriz){
+	for (int i = 1; i <= tamMatriz; ++i){
+		for (int j = 1; j <= tamMatriz; ++j){
+			printf("%d ", matrizArestas[i][j]);
+		}
+		printf("\n");
+	}
+}
 
-// 	return matrizArestas;
-// }
+int** alocaMatriz(int tamMatriz){
+	int **matriz;
 
-// char *verifica_serial_conflito(int **matrizArestas, int tamMatriz){
-// 	int ehSerial = 1;
+	matriz = (int**) malloc((tamMatriz + 1) * sizeof(int*));
 
-// 	for(int i = 0; i < tamMatriz; ++i){
-// 		for(int j = 0; j < tamMatriz; ++j){
-// 			if(matrizArestas[i][j] != 0){
-// 				if(matrizArestas[j][i] != 0){
-// 					ehSerial = 0;
-// 				}
-// 			}
-// 		}
-// 	}
+	for (int i = 1; i <= tamMatriz; ++i){
+		matriz[i] = (int*) malloc(tamMatriz * sizeof(int));
+		for (int j = 1; j <= tamMatriz; ++j){
+			matriz[i][j] = 0;
+		}
+	}
 
-// 	if (ehSerial){
-// 		return "SS";
-// 	} else {
-// 		return "NS";
-// 	}
-// }
-
-// void printMatriz(int** matrizArestas, int tamMatriz){
-// 	for (int i = 1; i < tamMatriz; ++i){
-// 		for (int j = 1; j < tamMatriz; ++j){
-// 			printf("%d ", matrizArestas[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-// }
+	return matriz;
+}
 
 void encontraEscalonamento(t_operacao *operacoes, int qtdeOperacoes, int *tempoInicio, int *tempoFim){
 	int i;
@@ -161,64 +154,78 @@ int cmpfunc(const void *a, const void *b) {
    return ( *(int*)a - *(int*)b );
 }
 
-char *encontraIdsEscalonamento(t_operacao *operacoes, int tempoInicio, int tempoFim){
-	int *ids, qtdeIds;
+char *encontraIdsEscalonamento(t_operacao *operacoes, int tempoInicio, int tempoFim, int *qtdeIds){
+	int *ids;
 	char *idsRetorno, *aux;
+	int auxQtdeIds;
 
 	ids = (int*) malloc(sizeof(int));
-	qtdeIds = 0;
+	auxQtdeIds = 0;
 
 	for(int i = tempoInicio; i <= tempoFim; ++i){
 		if(operacoes[i].opr == 'C'){
-			ids[qtdeIds] = operacoes[i].idT;
-			qtdeIds++;
+			ids[auxQtdeIds] = operacoes[i].idT;
+			auxQtdeIds++;
 			ids = (int*) realloc(ids, sizeof(int));
 		}
 	}
 
-	qsort(ids, qtdeIds, sizeof(int), cmpfunc);
+	qsort(ids, auxQtdeIds, sizeof(int), cmpfunc);
 
-	idsRetorno = (char*) malloc(qtdeIds * sizeof(char));
+	idsRetorno = (char*) malloc(auxQtdeIds * sizeof(char));
+	strcpy(idsRetorno, "");
 	aux = (char*) malloc(sizeof(char));
 
-	for (int i = 0; i < qtdeIds - 1; ++i){
+	for (int i = 0; i < auxQtdeIds - 1; ++i){
 		sprintf(aux, "%d", ids[i]);
 		strcat(idsRetorno, aux);
 		strcat(idsRetorno, ",");
 	}
 
-	sprintf(aux, "%d", ids[qtdeIds - 1]);
+	sprintf(aux, "%d", ids[auxQtdeIds - 1]);
 	strcat(idsRetorno, aux);
+
+	free(ids);
+	free(aux);
+
+	*qtdeIds = auxQtdeIds;
 
 	return idsRetorno;
 }
 
 int main(){
 	t_operacao *operacoes;
-	int qtdeOperacoes, tamMatriz, ehSerial, indexOpr, tamEscalonamento, numEscalonamento;
-	int **matrizArestas, tempoInicio, tempoFim;
+	int **matrizArestas, tempoInicio, tempoFim, qtdeCommits, qtdeOperacoes, numEscalonamento;
 	char *serialConfl, *idsEscalona, *resultado, *aux;
 
 	operacoes = leEntrada(&qtdeOperacoes);
 	tempoInicio = 1;
 	tempoFim = qtdeOperacoes;
 	resultado = (char *) malloc(sizeof(char));
-	aux = (char *) malloc(sizeof(char));
 	numEscalonamento = 1;
 
 	for (int i = tempoInicio; i < qtdeOperacoes; i += tempoFim){
 		encontraEscalonamento(operacoes, qtdeOperacoes, &tempoInicio, &tempoFim);
 
-		aux = encontraIdsEscalonamento(operacoes, tempoInicio, tempoFim);
+		aux = encontraIdsEscalonamento(operacoes, tempoInicio, tempoFim, &qtdeCommits);
 
+		matrizArestas = alocaMatriz(qtdeCommits);
+
+		serialConfl = serialConflito(operacoes,tempoInicio, tempoFim, matrizArestas, qtdeCommits);
 
 		sprintf(resultado, "%d", numEscalonamento);
 		strcat(resultado, " ");
 		strcat(resultado, aux);
+		strcat(resultado, " ");
+		strcat(resultado, serialConfl);
 
 		printf("%s\n", resultado);
 
+		for(int i = 1; i <= qtdeCommits; ++i){
+			free(matrizArestas[i]);
+		}
 
+		free(matrizArestas);
 
 		numEscalonamento++;
 		tempoInicio = tempoFim + 1;
